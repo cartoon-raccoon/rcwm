af74768e485d6bd31bce983d85527a407df48b26
cartoon-raccoon <shaughn.chan@gmail.com>
2021-02-12 02:19:05 +0800

    Added some code
    I hate this already

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 use xcb::base::Connection;
+use xcb_util::ewmh;
 
 fn main() {
-    let xconn = Connection::connect(None)
+    let (conn, screen_idx) = Connection::connect(None)
         .expect("Failed to connect to X server");
+    
+    let xconn = ewmh::Connection::connect(conn)
+        .map_err(|(err, _)| { err })
+        .expect("Failed to connect via EWMH");
 
+    
     
 }
