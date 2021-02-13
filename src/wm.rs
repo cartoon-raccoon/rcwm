c53bfa78bde18d9c64c31c342d69f3bcd90128a1
cartoon-raccoon <shaughn.chan@gmail.com>
2021-02-13 18:27:47 +0800

    Added derives and run function for WM

--- src/wm.rs
+++ src/wm.rs
@@ -2,13 +2,14 @@ use xcb_util::ewmh::Connection;
 
 use crate::xserver::{XConn, ROOT_ATTRS};
 
+#[derive(Clone, Copy)]
 pub struct WM<'a> {
     conn: XConn<'a>,
 }
 
 impl<'a> WM<'a> {
     pub fn register(conn: &'a Connection, screen_idx: i32) -> Self {
-        let mut xconn = XConn::new(conn);
+        let xconn = XConn::new(conn);
 
         let root_id = xconn.get_setup()
             .roots()
@@ -16,12 +17,31 @@ impl<'a> WM<'a> {
             .expect("Could not get root")
             .root();
 
-        xconn.change_window_attributes_checked(root_id, &ROOT_ATTRS);
+        // we panic here because this is a fatal error
+        // register for substructure redirect and substructure notify on root window 
+        xconn.change_window_attributes_checked(root_id, &ROOT_ATTRS)
+            .unwrap_or_else(|e| panic!("{}", e));
 
-        //xconn.set_supported(screen_idx, atoms: &[xcb::Atom]);
+        xconn.set_supported(screen_idx, &[
+            xconn.atoms.WM_PROTOCOLS, 
+            xconn.atoms.WM_DELETE_WINDOW
+        ]);
 
         Self {
             conn: xconn,
+        }
+    }
+
+    //using a mutable reference statically ensures there is only one instance running
+    pub fn run(&mut self) -> ! {
+        loop {
+            let event = self.conn.next_event();
+
+            match event.response_type() & !0x80 {
+                _ => {
+                    //handle event
+                }
+            }
         }
     }
 }
