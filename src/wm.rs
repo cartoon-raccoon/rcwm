cab1bfe511e81d2319090bcffdde324ae6c0b362
cartoon-raccoon <shaughn.chan@gmail.com>
2021-02-13 13:51:06 +0800

    Added xconn stuff

--- src/wm.rs
+++ src/wm.rs
@@ -1,6 +1,6 @@
 use xcb_util::ewmh::Connection;
 
-use crate::xserver::XConn;
+use crate::xserver::{XConn, ROOT_ATTRS};
 
 pub struct WM<'a> {
     conn: XConn<'a>,
@@ -9,6 +9,16 @@ pub struct WM<'a> {
 impl<'a> WM<'a> {
     pub fn register(conn: &'a Connection, screen_idx: i32) -> Self {
         let mut xconn = XConn::new(conn);
+
+        let root_id = xconn.get_setup()
+            .roots()
+            .nth(screen_idx as usize)
+            .expect("Could not get root")
+            .root();
+
+        xconn.change_window_attributes_checked(root_id, &ROOT_ATTRS);
+
+        //xconn.set_supported(screen_idx, atoms: &[xcb::Atom]);
 
         Self {
             conn: xconn,
