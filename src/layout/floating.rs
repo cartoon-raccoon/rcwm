02e213663c03d4b9e3bc2d1331497c9a17b599fd
cartoon-raccoon <shaughn.chan@gmail.com>
2021-03-28 02:29:16 +0800

    Implemented border colours on focus

--- src/layout/floating.rs
+++ src/layout/floating.rs
@@ -38,20 +38,28 @@ pub fn add_window(conn: &XConn, ws: &mut Workspace, screen: &Screen, window_id: 
     if let Some(focused) = ws.windows.focused() {
         conn.configure_window(window_id, &values::stack_above_sibling(focused.id()));
     }
+    conn.configure_window(window.id(), &[(xcb::CONFIG_WINDOW_BORDER_WIDTH as u16, 5)]);
 
     window.xwindow.update_geometry_conn(conn);
 
     match conn.query_pointer(screen.xwindow.id) {
         Ok(pointer) => {
             if pointer.child() == screen.xwindow.id || pointer.child() == window_id {
+                set_focus_colour(conn, pointer.child());
                 conn.set_input_focus(window_id);
+            } else {
+                if let Some(focused) = ws.windows.focused() {
+                    set_unfocus_colour(conn, focused.id());
+                } else {
+                    set_unfocus_colour(conn, window_id);
+                }
             }
         }
         Err(e) => {
             error!("{}", e)
         }
     }
-
+    
     conn.change_window_attributes(window.id(), &values::child_events());
 
     ws.windows.push(window);
@@ -85,6 +93,9 @@ pub fn del_window(
 pub fn window_focus(conn: &XConn, ws: &mut Workspace, window: XWindowID) {
 
     if let Some(idx) = ws.windows.contains(window) {
+        if let Some(focused) = ws.windows.focused() {
+            set_unfocus_colour(conn, focused.id());
+        }
         // internally focus
         ws.windows.move_front(idx);
         
@@ -94,6 +105,14 @@ pub fn window_focus(conn: &XConn, ws: &mut Workspace, window: XWindowID) {
         // tell x to focus
         window_stack_and_focus(ws, conn, window)
     }
+}
+
+fn set_focus_colour(conn: &XConn, window: XWindowID) {
+    conn.change_window_attributes(window, &[(xcb::CW_BORDER_PIXEL, 0xff0000)]);
+}
+
+fn set_unfocus_colour(conn: &XConn, window: XWindowID) {
+    conn.change_window_attributes(window, &[(xcb::CW_BORDER_PIXEL, 0xdddddd)]);
 }
 
 fn window_stack_and_focus(ws: &mut Workspace, conn: &XConn, window: XWindowID) {
@@ -107,6 +126,7 @@ fn window_stack_and_focus(ws: &mut Workspace, conn: &XConn, window: XWindowID) {
 
     // focus to current window
     conn.set_input_focus(window);
+    set_focus_colour(conn, window);
 
     // re-enable events
     conn.change_window_attributes(window, &values::child_events());
