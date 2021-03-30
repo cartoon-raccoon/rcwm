e1c2e2f8a819578a770ad21159d2d9959fd07251
cartoon-raccoon <shaughn.chan@gmail.com>
2021-03-31 00:25:31 +0800

    Removed debug logs

--- src/values.rs
+++ src/values.rs
@@ -17,17 +17,17 @@ pub const ROOT_BUTTON_GRAB_MASK: xcb::ButtonMask = xcb::EVENT_MASK_BUTTON_PRESS|
 pub const ROOT_POINTER_GRAB_MASK: xcb::EventMask = xcb::EVENT_MASK_BUTTON_RELEASE|xcb::EVENT_MASK_BUTTON_MOTION;
 
 pub fn cursor_attrs(cursor_id: u32) -> [(u32, u32); 1] {
-    debug!("Getting cursor attrs for cursor {}", cursor_id);
+    //debug!("Getting cursor attrs for cursor {}", cursor_id);
     return [(xcb::CW_CURSOR, cursor_id)]
 }
 
 pub fn disable_events() -> [(u32, u32); 1] {
-    debug!("VALUES: attributes no events");
+    //debug!("VALUES: attributes no events");
     return [(xcb::CW_EVENT_MASK, xcb::EVENT_MASK_NO_EVENT)];
 }
 
 pub fn child_events() -> [(u32, u32); 1] {
-    debug!("VALUES: attributes child events");
+    //debug!("VALUES: attributes child events");
     return [(xcb::CW_EVENT_MASK,
         xcb::EVENT_MASK_ENTER_WINDOW|    // -> Self EnterNotify events
         xcb::EVENT_MASK_STRUCTURE_NOTIFY // -> Self CirculateNotify, ConfigureNotify, DestroyNotify, GravityNotify, MapNotify, ReparentNotify, UnmapNotify events
@@ -35,17 +35,17 @@ pub fn child_events() -> [(u32, u32); 1] {
 }
 
 pub fn configure_move(x: u32, y: u32) -> [(u16, u32); 2] {
-    debug!("VALUES: configure move");
+    //debug!("VALUES: configure move");
     return [(xcb::CONFIG_WINDOW_X as u16, x), (xcb::CONFIG_WINDOW_Y as u16, y)];
 }
 
 pub fn configure_resize(width: u32, height: u32) -> [(u16, u32); 2] {
-    debug!("VALUES: configure resize");
+    //debug!("VALUES: configure resize");
     return [(xcb::CONFIG_WINDOW_WIDTH as u16, width), (xcb::CONFIG_WINDOW_HEIGHT as u16, height)];
 }
 
 pub fn stack_above_sibling(window_id: XWindowID) -> [(u16, u32); 2] {
-    debug!("VALUES: configure stack above sibling {}", window_id);
+    //debug!("VALUES: configure stack above sibling {}", window_id);
     return [
         (xcb::CONFIG_WINDOW_STACK_MODE as u16, xcb::STACK_MODE_ABOVE),
         (xcb::CONFIG_WINDOW_SIBLING as u16, window_id),
