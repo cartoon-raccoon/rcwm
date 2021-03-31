749da14c0d1372e42d9df8ddaeb49efd4a1c1c8b
cartoon-raccoon <shaughn.chan@gmail.com>
2021-04-01 00:23:10 +0800

    Finished implementing tiling algorithm
    Refactored the algorithm into a separate function with no side effects
    
    Deleting windows now retiles the window

--- src/layout/dtiled.rs
+++ src/layout/dtiled.rs
@@ -8,19 +8,32 @@ use super::BORDER_WIDTH;
 pub fn activate(conn: &XConn, ws: &mut Workspace, screen: &Screen) {
     // we cannot use the base activate function here as it cannot
     // account for when a new window is sent to the workspace from another
+
+    // if the workspace was previously empty but had a window sent to it
+    // the len is > 0 and the master is none
+    // if !ws.windows.is_empty() && ws.master.is_none() {
+    //     // set the first window as the master
+    //     debug!("Workspace contains window, but master is none - setting master");
+    //     ws.set_master(ws.windows.get(0).unwrap(/*this unwrap should be safe*/).id());
+    // }
     
-    
+    // reload the window layout
+    relayout(conn, ws, screen);
+    // now that all the geometries are calculated, we can map the normal way
     super::activate(conn, ws, screen)
 }
 
 pub fn deactivate(conn: &XConn, ws: &mut Workspace) {
+    // no special treatment needed for this one.
     super::deactivate(conn, ws)
 }
 
 pub fn add_window(conn: &XConn, ws: &mut Workspace, screen: &Screen, window_id: XWindowID) {
+    // Internally create a new window and set its supported protocols
     let mut window = Window::tiled(window_id);
     window.set_supported(conn);
 
+    // Get root geometries
     let root_geom = conn.get_root_geom().expect("Could not get root geometry");
     let root_geom2 = screen.xwindow.geom;
 
@@ -30,11 +43,104 @@ pub fn add_window(conn: &XConn, ws: &mut Workspace, screen: &Screen, window_id: 
         debug!("stored: {:?}", root_geom2);
     }
 
-    let pre_master = ws.master.is_some();
+    // Add new windows
+    // If there is already a master, insert after it
+    // Else, set new window as the master
+    if ws.master.is_some() {
+        debug!("dtiled::add_window: Pre-existing master, inserting after");
+        ws.windows.insert(1, window);
+    } else {
+        debug!("dtiled::add_window: No pre-existing master, pushing directly");
+        ws.set_master(window.id());
+        ws.windows.push(window);
+    }
 
+    // Calculate the tile sizes
+    calculate_geoms(ws, screen, root_geom);
+
+    for win in ws.windows.iter_mut() {
+        win.update_geometry(conn);
+        if win.id() == window_id {
+            conn.configure_window(win.id(), &[(xcb::CONFIG_WINDOW_BORDER_WIDTH as u16, BORDER_WIDTH)]);
+
+            conn.map_window(win.id());
+            conn.change_window_attributes(win.id(), &values::child_events());
+        }
+    }
+
+    
+
+}
+
+pub fn del_window(
+    conn: &XConn,
+    ws: &mut Workspace,
+    screen: &Screen,
+    window_id: XWindowID,
+    idx: usize
+) -> Window {
+    //todo: placeholder
+    debug!("Got window with idx {}", idx);
+
+    // internally remove window from tracking
+    let window = ws.windows.pop(idx);
+
+    // disable events and unmap the window
+    conn.change_window_attributes(window_id, &values::disable_events());
+    conn.unmap_window(window_id);
+
+    // set new workspace master or unset it if empty
+    if ws.is_master(window_id) {
+        debug!("dtiled::del_window: Window to destroy is master, doing unmap checks");
+        if ws.windows.len() == 0 {
+            debug!("dtiled::del_window: Workspace is now empty, unsetting master");
+            ws.unset_master(); //workspace is now empty
+            ws.windows.unset_focused();
+        } else {
+            debug!("dtiled::del_window: Workspace has {} windows, setting new master", ws.windows.len());
+            let new_master = ws.windows.get(0).unwrap().id();
+            debug!("New master is now {}", new_master);
+            ws.set_master(new_master);
+            debug!("Window at idx 0 is {:?}", ws.windows.get(0));
+            window_focus(conn, ws, new_master);
+        }
+    }
+
+    // recalculate layouts
+    relayout(conn, ws, screen);
+
+    for win in ws.windows.iter_mut() {
+        win.update_geometry(conn);
+    }
+
+    window
+
+    //super::floating::del_window(conn, ws, screen, window_id, idx)
+}
+
+pub fn window_focus(conn: &XConn, ws: &mut Workspace, window: XWindowID) {
+    // todo: placeholder
+    super::floating::window_focus(conn, ws, window)
+}
+
+pub fn toggle_floating(ws: &mut Workspace, window: XWindowID) {
+    if let Some(win) = ws.windows.lookup_mut(window) {
+        if win.is_tiled() {
+            win.set_floating();
+        } else {
+            win.set_tiled();
+        }
+    }
+}
+
+pub fn relayout(conn: &XConn, ws: &mut Workspace, screen: &Screen) {
+    let root_geom = conn.get_root_geom().expect("Could not get root geom");
+    calculate_geoms(ws, screen, root_geom);
+}
+
+fn calculate_geoms(ws: &mut Workspace, _screen: &Screen, root_geom: Geometry) {
+    // Calculate the tile sizes
     if let Some(mstr) = ws.master() {
-        debug!("Master exists, using tiling algo");
-
         if ws.is_empty() {
             error!("Workspace is empty but has a master window");
             return
@@ -44,16 +150,29 @@ pub fn add_window(conn: &XConn, ws: &mut Workspace, screen: &Screen, window_id: 
         // if the master window is the only window
         // it takes up the entire screen
         if ws.windows.len() == 1 {
-            debug!("Only master window onscreen");
+            debug!("dtiled::calculate_geoms: New window is master, tiling to full window");
+            // if there is no master window, this should mean the workspace is empty
+            // and we are mapping the master window
+            let master = ws.windows.lookup_mut(mstr).unwrap();
+
+            master.set_geometry(root_geom);
+
+        } else if ws.windows.len() == 2 {
+            debug!("dtiled::calculate_geoms: Only master window currently mapped");
+
+            // move master window to the front
+            let master_idx = ws.windows.contains(mstr).unwrap();
+            ws.windows.move_front(master_idx);
+            
             // get a mutable reference to the master window
             let master = ws.windows.lookup_mut(mstr).unwrap();
 
             // cut the master in half
-            let mut master_geom = master.xwindow.geom;
+            let mut master_geom = root_geom;
             master_geom.width = (master_geom.width / 2) - BORDER_WIDTH as i32;
             
-            // update its geometry
-            master.update_geometry(conn, master_geom);
+            // set its geometry
+            master.set_geometry(master_geom);
 
             // now for the slave window
 
@@ -76,29 +195,44 @@ pub fn add_window(conn: &XConn, ws: &mut Workspace, screen: &Screen, window_id: 
 
             dbg!(slave_geom);
 
-            window.update_geometry(conn, slave_geom);
+            let slave = ws.windows.get_mut(1).unwrap();
+
+            slave.set_geometry(slave_geom);
         } else { 
+            debug!("dtiled::calculate_geoms: Multiple windows mapped, calculating");
             // the master window is already chopped in half
             // and we need to update the slave windows instead
             // only update their height and xy coords
+
+            let master_idx = ws.windows.contains(mstr).unwrap();
+            ws.windows.move_front(master_idx);
 
             // get a mutable reference to the master window
             let master = ws.windows.lookup_mut(mstr).unwrap();
 
             // get master's geometry
-            let master_geom = master.xwindow.geom;
+            let mut master_geom = master.xwindow.geom;
+
+            if master_geom.x != 0 || master_geom.y != 0 {
+                debug!("Master is not in position");
+
+                master_geom = root_geom;
+                master_geom.width = (master_geom.width / 2) - BORDER_WIDTH as i32;
+
+                master.set_geometry(master_geom);
+            }
 
             // get no of slave windows
-            let slave_count = ws.windows.len() - 1 /*master*/ + 1 /*new slave*/;
+            let slave_count = ws.windows.len() - 1; //
 
             // calculate new height of all slave windows
             let slave_height = root_geom.height / slave_count as i32;
-
-            let mut idx = 0;
-            let mut win_width = master_geom.width / 2;
+            let slave_width = root_geom.width - master_geom.width;
 
             // for each window that is not a master
-            for (i, win) in ws.windows.iter_mut().filter(|win| win.id() != mstr).enumerate() {
+            for (i, win) in ws.windows.iter_mut()
+                .filter(|win| win.id() != mstr && win.is_tiled())
+                .enumerate() {
                 // calculate coords
                 let y = i as i32 * slave_height;
                 let x = master_geom.width;
@@ -107,101 +241,20 @@ pub fn add_window(conn: &XConn, ws: &mut Workspace, screen: &Screen, window_id: 
                     x: x,
                     y: y,
                     height: slave_height,
-                    width: win.width(),
+                    width: slave_width,
                 };
 
                 dbg!(slave_geom);
 
-                win.update_geometry(conn, slave_geom);
-                idx = i;
-                win_width = win.width();
+                win.set_geometry(slave_geom);
             }
 
-            let new_win_geometry = Geometry {
-                x: master_geom.width,
-                y: (idx as i32 + 1) * slave_height,
-                height: slave_height,
-                width: win_width,
-            };
-
-            window.update_geometry(conn, new_win_geometry);
+            
         }
 
 
 
     } else {
-        debug!("No master exists, tiling to full window");
-        // if there is no master window, this should mean the workspace is empty
-        // and we are mapping the master window
-        if !ws.is_empty() {
-            warn!("Workspace is not empty but has no master window");
-        }
-        window.update_geometry(conn, root_geom);
-        ws.set_master(window.id());
-
-        //todo: account for windows like status bars, etc.
+        assert!(ws.is_empty(), "Master is None but workspace is not empty")
     }
-
-    conn.configure_window(window.id(), &[(xcb::CONFIG_WINDOW_BORDER_WIDTH as u16, BORDER_WIDTH)]);
-
-    conn.map_window(window.id());
-    conn.change_window_attributes(window.id(), &values::child_events());
-
-    if pre_master {
-        debug!("Pre-existing master, inserting after");
-        ws.windows.insert(1, window);
-    } else {
-        debug!("No pre-existing master, pushing directly");
-        ws.windows.push(window);
-    }
-}
-
-pub fn del_window(
-    conn: &XConn,
-    ws: &mut Workspace,
-    screen: &Screen,
-    window_id: XWindowID,
-    idx: usize
-) -> Window {
-    //todo: placeholder
-    debug!("Got window with idx {}", idx);
-    let window = ws.windows.pop(idx);
-
-    conn.change_window_attributes(window_id, &values::disable_events());
-    conn.unmap_window(window_id);
-
-    if ws.is_master(window_id) {
-        debug!("Window to destroy is master, doing pre-unmap checks");
-        if ws.windows.len() == 0 {
-            debug!("Workspace is now empty, unsetting master");
-            ws.unset_master(); //workspace is now empty
-            ws.windows.unset_focused();
-        } else {
-            debug!("Workspace has {} windows, setting new master", ws.windows.len());
-            let new_master = ws.windows.get(0).unwrap().id();
-            debug!("New master is now {}", new_master);
-            ws.set_master(new_master);
-            debug!("Window at idx 0 is {:?}", ws.windows.get(0));
-            window_focus(conn, ws, new_master);
-        }
-    }
-
-    relayout(conn, ws, screen);
-
-    window
-
-    //super::floating::del_window(conn, ws, screen, window_id, idx)
-}
-
-pub fn window_focus(conn: &XConn, ws: &mut Workspace, window: XWindowID) {
-    // todo: placeholder
-    super::floating::window_focus(conn, ws, window)
-}
-
-pub fn toggle_floating() {
-
-}
-
-pub fn relayout(conn: &XConn, ws: &mut Workspace, screen: &Screen) {
-
 }
