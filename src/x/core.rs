4feee3a37a8efd067be0a5250a0cc3f153de89c8
cartoon-raccoon <shaughn.chan@gmail.com>
2021-04-11 18:07:02 +0800

    Moved independent items to separate mod
    Refactoring out the direct dependency on xcb, to loosely couple via a
    trait.
    
    The new core.rs file will contain dependency-free items, while the
    xserver file, to be renamed to xcb.rs, will contain the dependencies.

---
+++ src/x/core.rs
@@ -0,0 +1,158 @@
+use std::ops::Index;
+
+use xcb_util::ewmh;
+
+use crate::types::Geometry;
+
+pub use super::xserver::XConn;
+pub use super::event::*;
+
+pub type Atom = xcb::Atom;
+
+/// The list of atoms interned from the X Server by the WM.
+#[allow(non_snake_case)]
+#[derive(Clone, Copy)]
+pub struct InternedAtoms {
+    pub SUPPORTED: Atom,
+    
+    pub WM_DELETE_WINDOW: Atom,
+    pub WM_TAKE_FOCUS: Atom,
+
+    pub WM_PROTOCOLS: Atom,
+
+    pub WM_WINDOW_TYPE_DESKTOP: Atom,
+    pub WM_WINDOW_TYPE_DOCK: Atom,
+    pub WM_WINDOW_TYPE_TOOLBAR: Atom,
+    pub WM_WINDOW_TYPE_MENU: Atom,
+    pub WM_WINDOW_TYPE_UTILITY: Atom,
+    pub WM_WINDOW_TYPE_SPLASH: Atom,
+    pub WM_WINDOW_TYPE_DIALOG: Atom,
+    pub WM_WINDOW_TYPE_DROPDOWN_MENU: Atom,
+    pub WM_WINDOW_TYPE_NOTIFICATION: Atom,
+    pub WM_WINDOW_TYPE_NORMAL: Atom,
+    pub WM_STATE: Atom,
+}
+
+/// You really shouldn't be using this.
+impl Index<usize> for InternedAtoms {
+    type Output = Atom;
+
+    fn index(&self, idx: usize) -> &Atom {
+        match idx {
+            0 => &self.SUPPORTED,
+            1 => &self.WM_DELETE_WINDOW,
+            2 => &self.WM_PROTOCOLS,
+            3 => &self.WM_WINDOW_TYPE_NORMAL,
+            4 => &self.WM_WINDOW_TYPE_DIALOG,
+            5 => &self.WM_WINDOW_TYPE_TOOLBAR,
+            6 => &self.WM_WINDOW_TYPE_UTILITY,
+            7 => &self.WM_WINDOW_TYPE_SPLASH,
+            8 => &self.WM_STATE,
+            // yeah, if you index anything more than 8, you'll probably get a null pointer.
+            _ => &0,
+        }
+    }
+}
+
+impl InternedAtoms {
+    pub fn new(conn: &ewmh::Connection) -> Self {
+        Self {
+            SUPPORTED: conn.SUPPORTED(),
+
+            WM_DELETE_WINDOW: xcb::intern_atom(conn, false, "WM_DELETE_WINDOW")
+                .get_reply()
+                .unwrap_or_else(|e| panic!("{}", e))
+                .atom(),
+
+            WM_TAKE_FOCUS: xcb::intern_atom(conn, false, "WM_TAKE_FOCUS")
+                .get_reply()
+                .unwrap_or_else(|e| panic!("{}", e))
+                .atom(),
+            
+            WM_PROTOCOLS: conn.WM_PROTOCOLS(),
+            
+            WM_WINDOW_TYPE_DESKTOP: conn.WM_WINDOW_TYPE_DESKTOP(),
+            WM_WINDOW_TYPE_DOCK: conn.WM_WINDOW_TYPE_DOCK(),
+            WM_WINDOW_TYPE_TOOLBAR: conn.WM_WINDOW_TYPE_TOOLBAR(),
+            WM_WINDOW_TYPE_MENU: conn.WM_WINDOW_TYPE_MENU(),
+            WM_WINDOW_TYPE_UTILITY: conn.WM_WINDOW_TYPE_UTILITY(),
+            WM_WINDOW_TYPE_SPLASH: conn.WM_WINDOW_TYPE_SPLASH(),
+            WM_WINDOW_TYPE_DIALOG: conn.WM_WINDOW_TYPE_DIALOG(),
+            WM_WINDOW_TYPE_DROPDOWN_MENU: conn.WM_WINDOW_TYPE_DROPDOWN_MENU(),
+            WM_WINDOW_TYPE_NOTIFICATION: conn.WM_WINDOW_TYPE_NOTIFICATION(),
+            WM_WINDOW_TYPE_NORMAL: conn.WM_WINDOW_TYPE_NORMAL(),
+
+            WM_STATE: conn.WM_STATE(),
+        }
+    }
+}
+
+pub type XWindowID = xcb::Window;
+
+/// A low-level window type that contains a window ID and its geometry.
+#[derive(Debug, Clone, Copy)]
+pub struct XWindow {
+    pub id: XWindowID,
+    pub geom: Geometry,
+}
+
+impl PartialEq for XWindow {
+    #[inline(always)]
+    fn eq(&self, other: &Self) -> bool {
+        self.id == other.id
+    }
+}
+
+impl From<XWindowID> for XWindow {
+    fn from(from: XWindowID) -> Self {
+        Self {
+            id: from,
+            geom: Geometry::from((0,0,0,0))
+        }
+    }
+}
+
+impl XWindow {
+    /// Sets the geometry using a provided XConn.
+    pub fn set_geometry_conn(&mut self, conn: &XConn) {
+        match conn.get_geometry(self.id) {
+            Ok(geom) => {
+                debug!(
+                    "Updating geometry (conn):\nx: {}, y: {}, h: {}, w: {}", 
+                    geom.x, geom.y, geom.height, geom.width
+                );
+                self.geom = geom;
+            }
+
+            Err(e) => {
+                error!("{}", e);
+                return
+            }
+        }
+    }
+
+    /// Sets the geometry using a provided Geometry.
+    pub fn set_geometry(&mut self, geom: Geometry) {
+        debug!(
+            "Updating geometry for window {}:\nx: {}, y: {}, h: {}, w: {}", 
+            self.id, geom.x, geom.y, geom.height, geom.width
+        );
+        self.geom = geom;
+    }
+    /// Updates the width by a given difference.
+    pub fn update_width(&mut self, dx: i32) {
+        self.geom.width += dx;
+    }
+    /// Updates the height by given difference.
+    pub fn update_height(&mut self, dy: i32) {
+        self.geom.height += dy;
+    }
+    /// Updates the x coordinate of the window by a given difference.
+    pub fn update_pos_x(&mut self, dx: i32) {
+        self.geom.x += dx;
+    }
+    /// Updates the y coordinate of the window by a given difference.
+    pub fn update_pos_y(&mut self, dy: i32) {
+        self.geom.y += dy;
+    }
+}
--- src/x/mod.rs
+++ src/x/mod.rs
@@ -3,12 +3,11 @@
 //! and provides the basic methods for retrieving data from and setting data
 //! on the X server, that are called by other modules within RaccoonWM.
 
+pub mod core;
 pub mod xserver;
 pub mod ewmh;
 pub mod icccm;
 pub mod event;
-
-pub use xserver as core;
 
 pub use self::core::{XConn, XWindow, XWindowID, Atom};
 pub use self::event::XEvent;
